6710f7fcf79c6119e95177b200445ebba0b65351
Will Jenden <william.jenden@aeroinfo.com>
2018-08-30 13:36:48 -0700

    Add test cases

---
+++ test/examples/one_line_docstring.py
@@ -0,0 +1,10 @@
+"""one_line_docstring.py: This file has a docstring."""
+
+import os
+import sys
+
+def add_ten(number):
+    return number + 10
+
+if __name__ == '__main__':
+    [add_ten(i) for i in range(100)]
